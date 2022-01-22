107179060956e92bc74976d5da214cd2241192bf
dopewevmond <68214494+dopewevmond@users.noreply.github.com>
2022-01-22 02:05:34 +0000

    secret key, flash template

--- cackle/__init__.py
+++ cackle/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+import os
 
 app = Flask(__name__)
+app.secret_key = os.environ.get('SECRET_KEY')
 Bootstrap(app)
 
 from cackle import routes
--- cackle/routes.py
+++ cackle/routes.py
@@ -1,7 +1,17 @@
 from cackle import app
-from flask import render_template
+from flask import render_template, flash
 
 @app.route('/')
-@app.route('/index')
+@app.route('/home')
 def index():
-    return render_template('navbar.html')
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    flash('You need to login', 'danger')
+    return render_template('login.html')
+
+# verifying my secret key
+@app.route('/verify')
+def verify():
+    return '<p>{}</p>'.format(app.config['SECRET_KEY'])
