5058f49fc03314cc5ba82f51dba1808dda95a2a1
dopewevmond <68214494+dopewevmond@users.noreply.github.com>
2022-01-22 02:58:20 +0000

    login form

--- cackle/routes.py
+++ cackle/routes.py
@@ -1,15 +1,19 @@
 from cackle import app
-from flask import render_template, flash
+from flask import render_template, flash, redirect, url_for
+from cackle.forms import LoginForm
 
 @app.route('/')
 @app.route('/home')
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    flash('You need to login', 'danger')
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for {}, remember me={}'.format(form.username.data, form.remember_me.data), 'success')
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form)
 
 # verifying my secret key
 @app.route('/verify')
