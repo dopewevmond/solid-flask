87d1aa0e770750a2c5d1182d5b9874ed73c73397
dopewevmond <68214494+dopewevmond@users.noreply.github.com>
2022-01-23 18:33:49 +0000

    added signup/register functionality

--- cackle/forms.py
+++ cackle/forms.py
@@ -1,9 +1,26 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from cackle.models import User
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Enter password', validators=[DataRequired()])
     remember_me = BooleanField('Remember me')
     submit = SubmitField('Log In')
+
+
+class SignupForm(FlaskForm):
+    username = StringField('Type your preferred username', validators=[DataRequired()])
+    email = StringField('Email address', validators=[DataRequired(), Email()])
+    password = PasswordField('Create a password', validators=[DataRequired()])
+    password2 = PasswordField('Confirm password', validators=[DataRequired(), EqualTo('password', 'The passwords you entered do not match')])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        if User.query.filter_by(username=username.data).first():
+            raise ValidationError('This username is already in use. Select another')
+
+    def validate_email(self, email):
+        if User.query.filter_by(email=email.data).first():
+            raise ValidationError('This email is already in use. Select another')
--- cackle/routes.py
+++ cackle/routes.py
@@ -1,11 +1,12 @@
-from cackle import app
+from cackle import app, db
 from flask import render_template, flash, redirect, url_for
-from cackle.forms import LoginForm
-from flask_login import current_user, login_user
+from cackle.forms import LoginForm, SignupForm
+from flask_login import current_user, login_user, logout_user, login_required
 from cackle.models import User, Post
 
 @app.route('/')
 @app.route('/home')
+@login_required
 def index():
     return render_template('index.html')
 
@@ -20,9 +21,31 @@ def login():
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password', 'danger')
             return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         flash('Logged in successfully', 'success')
         return redirect(url_for('index'))
     return render_template('login.html', form=form)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = SignupForm()
+    if form.validate_on_submit():        
+        user = User(username=form.username.data)
+        user.email = form.email.data
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You successfully created an account', 'success')
+        return redirect(url_for('login'))
+    return render_template('signup.html', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    flash('Logged out successfully', 'success')
+    return redirect(url_for('index'))
 
 # verifying my secret key
 @app.route('/verify')
