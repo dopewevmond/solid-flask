8740afcea0b62fbe0cf31ee45915009744a28e99
dopewevmond <68214494+dopewevmond@users.noreply.github.com>
2022-01-23 05:01:18 +0000

    added login functionality

--- cackle/routes.py
+++ cackle/routes.py
@@ -1,6 +1,8 @@
 from cackle import app
 from flask import render_template, flash, redirect, url_for
 from cackle.forms import LoginForm
+from flask_login import current_user, login_user
+from cackle.models import User, Post
 
 @app.route('/')
 @app.route('/home')
@@ -9,9 +11,16 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    # if user is already logged in, redirect to index page
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for {}, remember me={}'.format(form.username.data, form.remember_me.data), 'success')
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password', 'danger')
+            return redirect(url_for('login'))
+        flash('Logged in successfully', 'success')
         return redirect(url_for('index'))
     return render_template('login.html', form=form)
 
