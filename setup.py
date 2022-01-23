78ea8558a2db27054d9d9f7a0f1763fd43c48c14
dopewevmond <68214494+dopewevmond@users.noreply.github.com>
2022-01-23 04:41:10 +0000

    configured shell context for easy debugging

--- setup.py
+++ setup.py
@@ -1,1 +1,6 @@
-from cackle import app
+from cackle import app, db
+from cackle.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
