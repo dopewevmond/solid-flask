ead273e9d39b6d510b17510dedaaaa9b2f746022
dopewevmond <68214494+dopewevmond@users.noreply.github.com>
2022-01-23 04:35:53 +0000

    reconfigured database to get rid of error

--- cackle/__init__.py
+++ cackle/__init__.py
@@ -1,13 +1,11 @@
 from flask import Flask
-import os
 from cackle.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.secret_key = os.environ.get('SECRET_KEY')
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from cackle import routes
+from cackle import routes, models
