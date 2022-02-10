cb3c06ae5a137f024c5f3e5569bdac04b9edc6f5
dopewevmond <68214494+dopewevmond@users.noreply.github.com>
2022-02-10 00:27:37 +0000

    installed flask moment

--- cackle/__init__.py
+++ cackle/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -14,5 +15,6 @@ login.login_view = 'login'
 login.login_message = 'You need to log in to access this page'
 login.login_message_category = 'warning'
 mail = Mail(app)
+moment = Moment(app)
 
 from cackle import routes, models, errors
