71d8b50764d59586d0202085245a875681178d71
dopewevmond <68214494+dopewevmond@users.noreply.github.com>
2022-02-27 17:05:55 +0000

    fixed the blueprint error handler

--- cackle/errors/handlers.py
+++ cackle/errors/handlers.py
@@ -2,11 +2,11 @@ from flask import render_template
 from cackle import db
 from cackle.errors import bp
 
-@bp.errorhandler(404)
+@bp.app_errorhandler(404)
 def not_found_error(error):
     return render_template('errors/error.html', type='404'), 404
 
-@bp.errorhandler(500)
+@bp.app_errorhandler(500)
 def internal_error(error):
     db.session.rollback()
     return render_template('errors/error.html', type='500'), 500
